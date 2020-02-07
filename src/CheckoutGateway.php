d97f436906c317e892956871a9bf5f9388cbade6
Benjamin Beganović <k1pstabug@gmail.com>
2020-02-07 03:06:52 +0100

    Generate getter & setters for default parameters

--- src/CheckoutGateway.php
+++ src/CheckoutGateway.php
@@ -7,5 +7,42 @@ use Omnipay\Common\AbstractGateway;
 
 class CheckoutGateway extends AbstractGateway
 {
-    // ..
+    /**
+     * Return the name of the gateway for the container.
+     *
+     * @return void
+     */
+    public function getName()
+    {
+        return 'Checkout';
+    }
+
+    public function getDefaultParameters()
+    {
+        return [
+            'secret_key' => '',
+            'public_key' => '',
+            'test_mode' => false,
+        ];
+    }
+
+    public function getSecretKey()
+    {
+        return $this->getParameter('secret_key');
+    }    
+
+    public function setSecretKey(string $value)
+    {
+        return $this->setParameter('secret_key', $value);
+    }
+
+    public function getPublicKey()
+    {
+        return $this->getParameter('public_key');
+    }
+
+    public function setPublicKey(string $value)
+    {   
+        return $this->setParameter('public_key', $value);
+    }
 }
