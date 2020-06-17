97a324b6903e2834dbbd1a81a3aa2c32efe617d2
Jordan Lane <jordan.lane@steinias.com>
2020-06-17 02:38:18 -0400

    First attempt at 3ds payment flow

--- src/Message/Purchase/PurchaseRequest.php
+++ src/Message/Purchase/PurchaseRequest.php
@@ -15,13 +15,25 @@ class PurchaseRequest extends AbstractRequest
     public function getData()
     {
         $this->validate('amount', 'currency');
-
         return [
+            'source' => ['type' => 'token', 'token' => $this->getToken()],
             'amount' => $this->getAmountInteger(),
             'currency' => strtoupper($this->getCurrency()),
             'description' => $this->getDescription(),
             'metadata' => $this->getMetaData(),
+            'reference' => $this->getTransactionId(),
+            'payment_ip' => $this->getClientIp(),
+            '3ds' => $this->get3DS(),
         ];
+    }
+    
+    public function get3DS() {
+        return $this->getParameter('3DS');
+    }
+
+    public function set3DS($value)
+    {
+        return $this->setParameter('3DS', $value);
     }
 
     public function getHttpMethod()
@@ -32,7 +44,6 @@ class PurchaseRequest extends AbstractRequest
     public function sendData($data)
     {
         $response = $this->sendRequest($data);
-
         return $this->response = new PurchaseResponse($this, $response);
     }
 
