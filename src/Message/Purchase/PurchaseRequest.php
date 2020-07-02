7d47fc5389feed507cb703c7532b12f6f0d391ee
Jordan Lane <jordan.lane@steinias.com>
2020-07-02 01:33:57 -0400

    Make purchase request support dynamic source

--- src/Message/Purchase/PurchaseRequest.php
+++ src/Message/Purchase/PurchaseRequest.php
@@ -15,8 +15,8 @@ class PurchaseRequest extends AbstractRequest
     public function getData()
     {
         $this->validate('amount', 'currency');
-        return [
-            'source' => ['type' => 'token', 'token' => $this->getToken()],
+
+        $data = [
             'amount' => $this->getAmountInteger(),
             'currency' => strtoupper($this->getCurrency()),
             'description' => $this->getDescription(),
@@ -25,18 +25,59 @@ class PurchaseRequest extends AbstractRequest
             'payment_ip' => $this->getClientIp(),
             '3ds' => $this->get3DS(),
         ];
+
+        $data['source'] = $this->getSourceData();
+
+        if ($this->getCustomerReference()) {
+            $data['customer'] = ['id' => $this->getCustomerReference()];
+        } elseif ($this->getBillingEmail()) {
+            $data['customer'] = ['email' => $this->getBillingEmail()];
+        }
+
+        return $data;
+    }
+
+    public function getSource(): ?array
+    {
+        return $this->getParameter('source');
+    }
+
+    public function setSource(array $value): self
+    {
+        return $this->setParameter('source', $value);
+    }
+
+    public function getBillingEmail(): ?string
+    {
+        return $this->getParameter('billingEmail');
+    }
+
+    public function setBillingEmail(string $value): self
+    {
+        return $this->setParameter('billingEmail', $value);
+    }
+
+    public function getCustomerReference(): ?string
+    {
+        return $this->getParameter('customerReference');
+    }
+
+    public function setCustomerReference(string $value): self
+    {
+        return $this->setParameter('customerReference', $value);
     }
     
-    public function get3DS() {
+    public function get3DS(): ?array
+    {
         return $this->getParameter('3DS');
     }
 
-    public function set3DS($value)
+    public function set3DS(array $value): self
     {
         return $this->setParameter('3DS', $value);
     }
 
-    public function getHttpMethod()
+    public function getHttpMethod(): string
     {
         return 'POST';
     }
@@ -50,5 +91,50 @@ class PurchaseRequest extends AbstractRequest
     public function getEndpoint()
     {
         return parent::getEndpoint() . '/payments';
+    }
+
+    private function getSourceData()
+    {
+        if ($this->getSource()) {
+            return $this->getSource();
+        } elseif ($this->getCardReference()) {
+            $source = ['type' => 'id', 'id' => $this->getCardReference()];
+            $card = $this->getCard();
+            if ($card !== null && $card->getCvv()) {
+                $source['cvv'] = $card->getCvv();
+            }
+            return $source;
+        } elseif ($this->getToken()) {
+            return ['type' => 'token', 'token' => $this->getToken()];
+        } elseif ($card = $this->getCard()) {
+            return [
+                'type' => 'card',
+                'number' => $card->getNumber(),
+                'expiry_month' => $card->getExpiryMonth(),
+                'expiry_year' => $card->getExpiryYear(),
+                'name' => $card->getName(),
+                'cvv' => $card->getCvv(),
+                'billing_address' => [
+                    'address_line1' => $card->getBillingAddress1(),
+                    'address_line2' => $card->getBillingAddress2(),
+                    'city' => $card->getBillingCity(),
+                    'state' => $card->getBillingState(),
+                    'zip' => $card->getBillingPostcode(),
+                    'country' => $card->getBillingCountry()
+                ],
+                'phone' => ['number' => $card->getPhone()],
+            ];
+        } elseif ($this->getCustomerReference() || $this->getBillingEmail()) {
+            $source = ['type' => 'customer'];
+            if ($this->getCustomerReference()) {
+                $source['id'] = $this->getCustomerReference();
+            } elseif ($this->getBillingEmail()) {
+                $source['email'] = $this->getBillingEmail();
+            }
+            return $source;
+        } else {
+            // one of source, cardReference, token, or card is required
+            $this->validate('source');
+        }
     }
 }
